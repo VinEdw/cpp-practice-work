4a52fd70e28f1475b0551126553073ba3074b783
Vincent Edwards <mister.man489@gmail.com>
2024-09-05 18:43:40 -0700

    Tax considerations added

--- 52-equilibrium-price-and-quantity/main.cpp
+++ 52-equilibrium-price-and-quantity/main.cpp
@@ -5,6 +5,8 @@ struct Coefficients {
   double intercept{};
 };
 
+// Section: Functions to get user input
+
 Coefficients get_coefficients() {
   Coefficients temp{};
 
@@ -16,42 +18,76 @@ Coefficients get_coefficients() {
   return temp;
 }
 
-double get_equilibrium_price(const Coefficients& supply, const Coefficients& demand) {
-  return (supply.slope * demand.intercept - demand.slope * supply.intercept) / (supply.slope - demand.slope);
+Coefficients get_supply_coefficients() {
+  std::cout << "For the supply curve...\n";
+  return get_coefficients();
 }
 
-double get_equilibrium_quantity(const Coefficients& supply, const Coefficients& demand) {
-  return (demand.intercept - supply.intercept) / (supply.slope - demand.slope);
+Coefficients get_demand_coefficients() {
+  std::cout << "For the demand curve...\n";
+  return get_coefficients();
 }
 
-double get_consumer_surplus(const Coefficients& supply, const Coefficients& demand) {
-  double eq_price { get_equilibrium_price(supply, demand) };
-  double eq_quantity { get_equilibrium_quantity(supply, demand) };
+double get_tax() {
+  std::cout << "Enter the tax: ";
+  double tax{};
+  std::cin >> tax;
+  return tax;
+}
+
+// Section: Functions to calculate important quantities 
+
+double get_equilibrium_quantity(const Coefficients& supply, const Coefficients& demand, double tax=0.0) {
+  return (demand.intercept - supply.intercept - tax) / (supply.slope - demand.slope);
+}
+
+double get_equilibrium_price(const Coefficients& supply, const Coefficients& demand, double tax=0.0) {
+  return (supply.slope * demand.intercept - demand.slope * (supply.intercept + tax)) / (supply.slope - demand.slope);
+}
+
+double get_price_producers_receive(const Coefficients& supply, const Coefficients& demand, double tax=0.0) {
+  return get_equilibrium_price(supply, demand, tax) - tax;
+}
+
+double get_consumer_surplus(const Coefficients& supply, const Coefficients& demand, double tax=0.0) {
+  double eq_price { get_equilibrium_price(supply, demand, tax) };
+  double eq_quantity { get_equilibrium_quantity(supply, demand, tax) };
   return 0.5 * (demand.intercept - eq_price) * eq_quantity;
 }
 
-double get_producer_surplus(const Coefficients& supply, const Coefficients& demand) {
-  double eq_price { get_equilibrium_price(supply, demand) };
-  double eq_quantity { get_equilibrium_quantity(supply, demand) };
-  return 0.5 * (eq_price - supply.intercept) * eq_quantity;
+double get_producer_surplus(const Coefficients& supply, const Coefficients& demand, double tax=0.0) {
+  double price_producers_receive { get_price_producers_receive(supply, demand, tax)};
+  double eq_quantity { get_equilibrium_quantity(supply, demand, tax) };
+  return 0.5 * (price_producers_receive - supply.intercept) * eq_quantity;
 }
 
+double get_government_revenue(const Coefficients& supply, const Coefficients& demand, double tax=0.0) {
+  return get_equilibrium_quantity(supply, demand, tax) * tax;
+}
+
+double get_dead_weight_loss(const Coefficients& supply, const Coefficients& demand, double tax=0.0) {
+  double eq_quantity { get_equilibrium_quantity(supply, demand, tax) };
+  double eq_quantity_no_tax { get_equilibrium_quantity(supply, demand, 0.0) };
+  return 0.5 * tax * (eq_quantity_no_tax - eq_quantity);
+}
+
+// Section: main
+
 int main() {
-  std::cout << "This model assumes that the supply and demand curves are both linear.\n";
+  std::cout << "This model assumes that the supply and demand curves are both linear, and that a constant tax may added on the producers.\n";
+
+  Coefficients supply { get_supply_coefficients() };
+  Coefficients demand { get_demand_coefficients() };
+  double tax { get_tax() };
 
   std::cout << '\n';
-  std::cout << "For the supply curve...\n";
-  Coefficients supply { get_coefficients() };
-
-  std::cout << '\n';
-  std::cout << "For the demand curve...\n";
-  Coefficients demand { get_coefficients() };
-
-  std::cout << '\n';
-  std::cout << "Equilibrium Quantity: " << get_equilibrium_quantity(supply, demand) << '\n';
-  std::cout << "Equilibrium Price: " << get_equilibrium_price(supply, demand) << '\n';
-  std::cout << "Consumer Surplus: " << get_consumer_surplus(supply, demand) << '\n';
-  std::cout << "Producer Surplus: " << get_producer_surplus(supply, demand) << '\n';
+  std::cout << "Equilibrium Quantity: " << get_equilibrium_quantity(supply, demand, tax) << '\n';
+  std::cout << "Equilibrium Price: " << get_equilibrium_price(supply, demand, tax) << '\n';
+  std::cout << "Price Producers Receive: " << get_price_producers_receive(supply, demand, tax) << '\n';
+  std::cout << "Consumer Surplus: " << get_consumer_surplus(supply, demand, tax) << '\n';
+  std::cout << "Producer Surplus: " << get_producer_surplus(supply, demand, tax) << '\n';
+  std::cout << "Government Revenue: " << get_government_revenue(supply, demand, tax) << '\n';
+  std::cout << "Dead Weight Loss: " << get_dead_weight_loss(supply, demand, tax) << '\n';
 
   return 0;
 }
