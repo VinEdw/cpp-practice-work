fb5a6b1a80f30703f58276b8d336be2330b3501b
Vincent Edwards <mister.man489@gmail.com>
2024-10-02 00:31:52 -0700

    Program created to find indices of max and min values in a vector

---
+++ 65-vector-min-and-max/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+template <typename T>
+std::pair<std::size_t, std::size_t>find_min_max_indices(const std::vector<T>& arr) {
+  std::size_t min_idx { 0 };
+  std::size_t max_idx { 0 };
+  for (std::size_t idx { 0 }; idx < arr.size(); ++idx) {
+    if (arr[idx] > arr[max_idx]) {
+      max_idx = idx;
+    }
+    else if (arr[idx] < arr[min_idx]) {
+      min_idx = idx;
+    }
+  }
+  return { min_idx, max_idx };
+}
+
+template <typename T>
+void print_vector(const std::vector<T>& arr) {
+  std::cout << "( ";
+  for (std::size_t i { 0 }; i < arr.size(); ++i) {
+    if (i == 0) {
+      std::cout << arr[i];
+    }
+    else {
+      std::cout << ", " << arr[i];
+    }
+  }
+  std::cout << " )";
+}
+
+template <typename T>
+void print_min_max(const std::vector<T>& arr) {
+  std::cout << "With array ";
+  print_vector(arr);
+  std::cout << ":\n";
+
+  auto indices { find_min_max_indices(arr) };
+  std::cout << "The min element has index " << indices.first << " and value " << arr[indices.first] << '\n';
+  std::cout << "The max element has index " << indices.second << " and value " << arr[indices.second] << '\n';
+}
+
+int main() {
+  std::vector v1 { 3, 8, 2, 5, 7, 8, 3 };
+  print_min_max(v1);
+
+  std::cout << '\n';
+
+  std::vector v2 { 5.5, 2.7, 3.3, 7.6, 1.2, 8.8, 6.6 };
+  print_min_max(v2);
+
+  return 0;
+}
