ec01206ffd4ea2dde851e089736634e5ef725bf4
Vincent Edwards <mister.man489@gmail.com>
2024-10-09 21:26:12 -0700

    Reformatted code

--- 67-arr-passing/main.cpp
+++ 67-arr-passing/main.cpp
@@ -14,11 +14,11 @@ void printArray(const std::array<T, N>& arr) {
 }
 
 int main() {
-    constexpr std::array arr1 { 1, 4, 9, 16 };
-    printArray(arr1);
+  constexpr std::array arr1 { 1, 4, 9, 16 };
+  printArray(arr1);
 
-    constexpr std::array arr2 { 'h', 'e', 'l', 'l', 'o' };
-    printArray(arr2);
+  constexpr std::array arr2 { 'h', 'e', 'l', 'l', 'o' };
+  printArray(arr2);
 
-    return 0;
+  return 0;
 }
