a2e5c18c2a985187b5c3bf6262dab0bcb7c48808
Vincent Edwards <mister.man489@gmail.com>
2025-08-02 15:26:59 -0700

    Start playing around with dynamic memory allocation

---
+++ 78-dynamic-memory-intro/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <new>
+
+int main() {
+  int* ptr { new int{} }; // Dynamically allocate an integer and assign the address to ptr
+  *ptr = 7;               // Assign a value to the allocated memory
+
+  std::cout << *ptr << '\n';
+
+  delete ptr;             // Return the memory pointed to by ptr to the operating system
+  ptr = nullptr;          // Set ptr to be a null pointer
+
+  // In case the operating system does not have any memory to grant the request with
+  int* value { new (std::nothrow) int{} }; // value will be set to a null pointer if the integer allocation fails
+  // Handle case where new returned null
+  if (!value) {
+    std::cerr << "Could not allocate memory\n";
+  }
+
+  return 0;
+}
